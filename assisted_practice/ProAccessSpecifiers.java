cbe278724b6aa0260b1b1103f96e4a9e0257d558
Sinchanapd <65004762+Sinchanapd@users.noreply.github.com>
2022-03-06 23:49:46 +0530

    Delete assisted_practice directory

--- assisted_practice/ProAccessSpecifiers.java
+++
@@ -1,9 +0,0 @@
-package assisted_practice;
-
-public class ProAccessSpecifiers {
-	protected void display() 
-    { 
-        System.out.println("This is protected access specifier"); 
-    } 
-
-}
--- assisted_practice/PubAccessSpecifiers.java
+++
@@ -1,9 +0,0 @@
-package assisted_practice;
-
-public class PubAccessSpecifiers {
-	public void display() 
-    { 
-        System.out.println("This is Public Access Specifiers"); 
-    } 
-
-}
