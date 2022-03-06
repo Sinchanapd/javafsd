6ffa4fce9cfdc959a35e714d6fabf4aa435e3170
Sinchanapd <65004762+Sinchanapd@users.noreply.github.com>
2022-03-06 23:50:01 +0530

    Delete assisted_practice1 directory

--- assisted_practice1/AccessSpecifiers3.java
+++
@@ -1,9 +0,0 @@
-package assisted_practice1;
-
-import assisted_practice.*;
-public class AccessSpecifiers3 extends ProAccessSpecifiers {
-	public static void main(String[] args) {
-		AccessSpecifiers3 obj = new AccessSpecifiers3 ();   
-	       obj.display();  
-	}
-}
