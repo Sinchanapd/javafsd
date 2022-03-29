0275bb9e216f0d9498b3375f7f6703ebfa46899a
Sinchanapd <65004762+Sinchanapd@users.noreply.github.com>
2022-03-30 00:38:55 +0530

    Add files via upload

---
+++ Phase2/main project/FlyAway/WebContent/ULogin.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>User Login</title>
+</head>
+<body>
+      <div>   
+    <b>Welcome to FlyAway User  Login</b> 
+     <form action="UserLogin" method="post">
+        Name <input name="name" type="text"><br>
+        Email <input name="email" type="text"><br>
+        <button type="submit" value="Submit">LogIn</button>    
+     </form>   
+    </div>
+</body>
+</html>
---
+++ Phase2/main project/FlyAway/src/com/flyaway/AdminLogin.java
@@ -0,0 +1,46 @@
+package com.flyaway;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.URegistration.dao.AdminLogindao;
+import com.URegistration.dao.Logindao;
+
+/**
+ * Servlet implementation class AdminLogin
+ */
+@WebServlet("/AdminLogin")
+public class AdminLogin extends HttpServlet {
+	
+	public static boolean isLoggedIn = false;
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String email = request.getParameter("email");
+		String pass = request.getParameter("pass");
+		
+		PrintWriter out = response.getWriter();
+		
+		//out.println(email+" "+pass);
+		
+		
+		AdminLogindao logindao = new AdminLogindao();
+		if(logindao.check(email,pass)){
+			   isLoggedIn = true;
+	            out.println("You have LoggedIn");
+	            response.sendRedirect("AdminDashboard.jsp");
+			
+		}
+		else{
+			    isLoggedIn = false;
+	            out.println("Login Failed : Incorrect email or Password");
+		}	
+	}
+}
