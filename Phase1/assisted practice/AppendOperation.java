34153003ff1914139ac42eb7196bd6eca546ca82
Sinchanapd <65004762+Sinchanapd@users.noreply.github.com>
2022-04-25 23:21:14 +0530

    Add files via upload

--- Phase1/assisted practice/AppendOperation.java
+++ Phase1/assisted practice/AppendOperation.java
@@ -5,11 +5,11 @@ import java.io.*;
 public class AppendOperation {
 
 	public static void main(String[] args) {
-		//String path="F://Mphasis//assignment//test.txt";
+		//String path="C://Users//HP//Downloads//test.txt";
 		//String msg="Appending some data";
 		try
 		{
-			FileWriter file=new FileWriter("F://Mphasis//Phase1//assignment//test.txt",true);
+			FileWriter file=new FileWriter("C://Users//HP//Downloads//test.txt",true);
 			file.write("Appending some data");
 			file.close();
 			System.out.println("Append operation successful");
