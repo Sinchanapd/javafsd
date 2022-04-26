84c50058533652a8846df2ee0adf071795f71c3c
Sinchanapd <65004762+Sinchanapd@users.noreply.github.com>
2022-04-26 22:16:35 +0530

    Add files via upload

--- Phase5/practice project/AutomateAWebApplication/src/com/simplilearn/Registration.java
+++ Phase5/practice project/AutomateAWebApplication/src/com/simplilearn/Registration.java
@@ -11,12 +11,12 @@ public class Registration {
 
 	public static void main(String[] args) {
 		
-		String path= "F:\\Phase5\\chromedriver_win32\\chromedriver.exe";
-		System.setProperty("webdriver.chrome.driver",path);
+	String path= "F:\\Phase5\\chromedriver_win32\\chromedriver.exe";
+	System.setProperty("webdriver.chrome.driver",path);
 		
-		WebDriver driver= new ChromeDriver();
-		String base_url= "https://www.shine.com/registration/";
-		driver.get(base_url);
+	WebDriver driver= new ChromeDriver();
+	String base_url= "https://www.shine.com/registration/";
+	driver.get(base_url);
         
         WebElement name= driver.findElement(By.id("id_name"));
         name.sendKeys("Sinchana");
@@ -36,36 +36,5 @@ public class Registration {
         
         WebElement button= driver.findElement(By.id("registerButton"));
         button.click();
-		
-		
-	/*	WebElement first= driver.findElement(By.name("firstname"));
-		first.sendKeys("Sin");
-		
-		WebElement last= driver.findElement(By.name("lastname"));
-		last.sendKeys("P");
-		
-		WebElement email= driver.findElement(By.name("reg_email__"));
-		email.sendKeys("sin@gmail.com");
-		
-		WebElement confirm= driver.findElement(By.name("reg_email_confirmation__"));
-		confirm.sendKeys("sin@gmail.com");
-			
-		WebElement password= driver.findElement(By.name("reg_passwd__"));
-		password.sendKeys("sinchu");
-		
-		Select day = new Select(driver.findElement(By.id("day")));
-		day.selectByVisibleText("26");
-		
-		Select month = new Select(driver.findElement(By.id("month")));
-		month.selectByVisibleText("Nov");
-		
-		Select year = new Select(driver.findElement(By.id("year")));
-		year.selectByVisibleText("1997");
-		
-		driver.findElement(By.xpath("//input[@value='1']")).click();
-		
-		WebElement register= driver.findElement(By.name("websubmit"));
-		register.click();
-	*/
-	}
+     }
 }
